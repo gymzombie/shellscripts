fd90761b3373a0c2c4cd0872ac636dd3d0066657
gymzombie <gymzombie@users.noreply.github.com>
2016-03-18 12:06:02 -0400

    Update unixadmincron.sh

--- unixadmincron.sh
+++ unixadmincron.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Need to create a script that does nightly (weekly?) upgrades, patches, reviews, and emails 
-# Sysadmin to keep the status awared. 
+# Sysadmin to keep the status awared. FreeBSD does a good job of this, and wanted to bring some equivalent
+# to my Linux Boxes
+
+# Make sure cron sends results to admin...
 
 apt-get upgrade
 apt-get dist-upgrade
 apt-get update
 
 updatedb
+
+df -h
 
 
 
