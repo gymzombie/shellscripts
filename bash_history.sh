ffe362c0d4e1cddf91f7d724f29f1dac093d89fc
gymzombie <gymzombie@users.noreply.github.com>
2016-03-31 12:05:05 -0400

    Update bash_history.sh
    Added some stuff from @climagic
    
    https://twitter.com/climagic
    
    This is just March 2016.

--- bash_history.sh
+++ bash_history.sh
@@ -57,8 +57,25 @@ tail /var/log/lastlog
 tail /var/log/syslog
 sudo tail /var/log/syslog
 python -m SimpleHTTPServer 
-
-
+sudo bash -c 'swapoff -a && swapon -a' # After killing processing using up all your RAM on Linux (Firefox!!), move processes off of swap.
+sudo fdisk -l /dev/sd? # Show partitions of only physical SCSI/SATA/SCA drives and not other things like device mapper entries.
+<Ctrl-r>searchpattern # Every time you reuse a command from your command history, it saves a tree in the digital world. 
+echo ${USER:0:${#USER}/2} # You can use arithmetic inside bash variable expansions, such as this to get the first half of your username
+touch -r oldfile newfile # set access/modification times of newfile to those of oldfile. #ididntdoit
+echo ｙoυrｂａｎｋ.com | hexdump -c # Check a domain to make sure its all the ASCII characters you expect and not Unicode look-a-likes
+lsof -Pan -i tcp -i udp # Show all programs listening TCP/UDP ports. You may want to run this with superuser privileges.
+for i in $(seq 4 65);do echo -n "${pi:0:$i}.com: ";whois ${pi:2:$((i-2))}.com|grep --col=no "rant Na"||echo;sleep 5;done #pi domain owners
+pi=$( echo "scale=1000;4*a(1)" | BC_LINE_LENGTH=1004 bc -l ) # Happy Pi Day! Calculate π to 1000 digits using bc and assign it to pi
+ps ax -o state -o ppid | awk '$1=="Z"{print $2}' | xargs kill -9 # Kill all #zombies on the system.
+echo "$URL" | tr -s / _ # Change URL into a more filesystem friendly form and squeeze the repeated _ chars into one. #TryingAgain
+seq 20 | shuf # Generate a random ordered list of 20 numbers. For example to determine order of presentation.
+find . -exec file -b --mime-type {} + | sort | uniq -c | sort -nr # Make stats of the top file types in this directory and below.
+grep -io "cyber" | wc -l # How many times did they say cyber? Just select all in your browser copy and paste into this command to find out.
+shopt -u huponexit # in bash, set this option to make sure backgrounded jobs don't get killed when the parent shell exits
+pgrep rsync | xargs ionice -c3 -p # Adjust all rsync processes on the system so that they have lower (idle) IO priority.
+grep -h "POST /.*wp-login.php" *-access_log |awk '$1!~/^my.ip.addr$/{print $1}' |sort|uniq -c|sort -nr |head -50> wp-abusers.txt # WP abuse
+ps wu -U someuser # An easy way of selecting the processes owned by someuser.
+(head -5; tail -5) < log # Show the first and last 5 lines of the file 'log'.
 
 
 
